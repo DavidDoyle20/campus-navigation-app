ec248daf13e59afa82bec94be9fd130d55f56ce4
DavidDoyle20 <davestar80@gmail.com>
2025-02-03 10:22:19 -0600

    added files for deployment

--- campusnavigationapp/settings.py
+++ campusnavigationapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@5mpjic(opu_=er=7kyqy28*^=dv8b!@@$(zghm8fh2_ai&^ca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
