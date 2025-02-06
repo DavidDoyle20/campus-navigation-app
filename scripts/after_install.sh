ae807f5fd7e845224957d036704567f772e147ff
DavidDoyle20 <davestar80@gmail.com>
2025-02-06 15:54:10 -0600

    updated deployment scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,16 +5,22 @@ sudo pkill -f runserver
 
 cd /home/ubuntu/campus-navigation-app/
 
-# Update system and install GDAL dependencies
-sudo apt-get update
-sudo apt-get install -y gdal-bin libgdal-dev
-
 # activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-install requirements.txt
+# Install requirements with GDAL first
+pip install --no-cache-dir "GDAL==$GDAL_VERSION.*"
 pip install -r /home/ubuntu/campus-navigation-app/requirements.txt
+
+# Configure GDAL paths
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+GDAL_VERSION=$(gdal-config --version | awk -F'[.]' '{print $1"."$2}')
+
+
+# Django setup
+python manage.py migrate
 
 # run server
 screen -d -m python3 manage.py runserver 0:8000
