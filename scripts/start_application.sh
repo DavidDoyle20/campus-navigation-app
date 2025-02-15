250b5bea645f82a6056797e5b189a223f0142fdb
DavidDoyle20 <davestar80@gmail.com>
2025-02-14 20:25:58 -0600

    fixed start_application script not properly handling running gunicorn daemon

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/campus-navigation-app
 source venv/bin/activate
+
+# Stop existing Gunicorn process
+pkill gunicorn
+
+# Start Gunicorn
 gunicorn campusnavigationapp.wsgi:application --bind 0.0.0.0:8000 --daemon
+
+# Restart Nginx
 sudo systemctl restart nginx
